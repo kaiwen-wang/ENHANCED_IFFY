ae0575cff59bac4a93520236cfa4f15f61cbe8a6
Kaiwen Wang <kvnwkw@gmail.com>
2024-09-20 16:47:15 -0400

    redirect

--- src/routes/subscribe/+page.svelte
+++ src/routes/subscribe/+page.svelte
@@ -2,8 +2,9 @@
     import { onMount } from "svelte";
 
     onMount(() => {
-        window.location.href = "https://www.google.com";
+        window.location.href =
+            "https://pay.iffy.com/c/pay/cs_live_b1UGQUf0KpdJ9g0KfYp6XUvCBy53wq8Ryhc3X8TB6G6E0xh1dRPNqZfav2#fid2cGd2ZndsdXFsamtQa2x0cGBrYHZ2QGtkZ2lgYSc%2FY2RpdmApJ2R1bE5gfCc%2FJ3VuWmlsc2BaMDRVU2FmZkB3X3VzSHZyTkFtS09xal9hS258XWIzUDxHNHUyZGY0YlZRYUBvdEBQbEtjUU93Q3dQTmh0VTE9X1ddUF0xUXIyTVdGVHNyXGxvaHVgfHJfanI1NV8xS2pHUm5KJyknY3dqaFZgd3Ngdyc%2FcXdwYCknaWR8anBxUXx1YCc%2FJ2hwaXFsWmxxYGgnKSdga2RnaWBVaWRmYG1qaWFgd3YnP3F3cGB4JSUl";
     });
 </script>
 
-<div>Redirecting to Google...</div>
+<div>Redirecting...</div>
